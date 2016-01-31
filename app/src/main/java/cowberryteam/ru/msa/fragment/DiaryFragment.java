724690a6a77757371cec6c844fc6070b06cb3cc5
Vladimir <linuxdead@yandex.ru>
2016-01-31 14:01:25 +0300

    name

--- app/src/main/java/cowberryteam/ru/msa/fragment/DiaryFragment.java
+++ app/src/main/java/cowberryteam/ru/msa/fragment/DiaryFragment.java
@@ -22,6 +22,6 @@ public class DiaryFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_diary_mon, container, false);
+        return inflater.inflate(R.layout.fragment_diary, container, false);
     }
 }
